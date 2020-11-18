2581d08b0816e8838b1d06d6920915abb2b1f3de
Jay Arora <arorajay640@gmail.com>
2020-11-18 13:23:10 -0500

    adding transaction to the check Database function

--- public/db.js
+++ public/db.js
@@ -23,7 +23,7 @@ request.onerror = function (event) {
 
 function saveRecord(record) {
   // create a transaction on the pending db with readwrite access
-  const transcation = db.transcation(["pending"], "readwrite");
+  const transaction = db.transaction(["pending"], "readwrite");
   // access your pending object store
   const store = db.objectStore("pending");
   // add record to your store with add method.
@@ -33,8 +33,11 @@ function saveRecord(record) {
 
 function checkDatabase() {
   // open a transaction on your pending db
+  const transaction = db.transaction(["pending"], "readwrite");
   // access your pending object store
+  const store = transaction.onjectStore("pending");
   // get all records from store and set to a variable
+  const getAll = store.getAll();
 
   getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
